32d3f334dbf2e8e4a7ea9e02bc348fc6aeac4241
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2023-07-17 18:35:06 +0530

    Create Remove Character.cpp

---
+++ Remove Character.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h> 
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+string removeAllOccurrencesOfChar(string input, char c) {
+    string ans;
+    for(int i=0;i<input.size();i++){
+        if(input[i]!=c) ans.push_back(input[i]);
+    }
+    return ans;
+}
+
+int main() {
+    string input;
+    char c;
+    cin >> input >> c;
+    string output = removeAllOccurrencesOfChar(input, c);
+    cout << output << endl;
+    return 0;
+}
