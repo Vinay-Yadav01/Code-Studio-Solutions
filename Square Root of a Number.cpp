633da370a90066f1229fea77add67b6381a81abb
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2023-08-14 19:37:07 +0530

    Create Square Root of a Number.cpp

---
+++ Square Root of a Number.cpp
@@ -0,0 +1,15 @@
+int floorSqrt(int n)
+{
+  // Write your code here.
+  int low=1,high=n;
+  int ans=1;
+  while(low<=high){
+    long long mid=(low+high)/2;
+    if (mid * mid <= n) {
+      ans = mid;
+      low = mid + 1;
+    }
+    else high=mid-1;
+  }
+  return ans;
+}
