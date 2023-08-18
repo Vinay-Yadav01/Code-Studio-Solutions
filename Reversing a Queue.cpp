89a537ffd4bb814956b59942d0e672b1f9e335df
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2023-08-18 19:39:07 +0530

    Create Reversing a Queue.cpp

---
+++ Reversing a Queue.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h> 
+queue<int> reverseQueue(queue<int> q)
+{
+    queue<int> ans;
+    stack<int> s;
+    while(!q.empty()){
+        s.push(q.front());
+        q.pop();
+    }
+    while(!s.empty()){
+        ans.push(s.top());
+        s.pop();
+    }
+    return ans;
+}
