b6284fc186f2ba0abd90348b643c1d561f6f21f5
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2023-08-19 17:54:26 +0530

    Create First Negative In Every Window.cpp

---
+++ First Negative In Every Window.cpp
@@ -0,0 +1,44 @@
+#include <queue>
+vector<int> firstNegative(vector<int> arr, int n, int k) {
+	deque<int> dq;
+    vector<int> ans;
+    int negative = -1;
+
+    //process first window
+    for(int i=0; i<k; i++) {
+        if(arr[i] < 0) {
+            dq.push_back(i);
+        }
+    }
+
+    //push ans for FIRST window
+    if(dq.size() > 0) {
+        ans.push_back(arr[dq.front()]);
+    }
+    else
+    {
+        ans.push_back(0);
+    }
+
+    //now process for remaining windows
+    for(int i = k; i<n; i++) {
+        //first pop out of window element       
+        if(!dq.empty() && (i - dq.front())>=k ) {
+            dq.pop_front();
+        }
+        
+        //then push current element
+        if(arr[i] < 0)
+        dq.push_back(i);
+        
+    // put in ans
+    if(dq.size() > 0) {
+            ans.push_back(arr[dq.front()]);
+    }
+    else
+    {
+        ans.push_back(0);
+    }
+    }
+    return ans;
+}
